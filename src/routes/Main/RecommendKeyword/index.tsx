282b75e268d17269fa187480300fec2d7374091e
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-19 22:15:54 +0900

    feat : 키보드로 선택 및 mark태그 강조 추가

--- src/routes/Main/RecommendKeyword/index.tsx
+++ src/routes/Main/RecommendKeyword/index.tsx
@@ -1,6 +1,8 @@
-import { Dispatch, MouseEvent, SetStateAction } from 'react'
+import styles from './RecommendKeyword.module.scss'
+import { Dispatch, MouseEvent, SetStateAction, useCallback, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from 'states'
+import cx from 'classnames'
 
 import useKeywordQuery from 'hooks/useKeywordQuery'
 import { useAppDispatch } from 'hooks/useAppDispatch'
@@ -13,6 +15,8 @@ interface IProps {
 }
 
 const RecommendKeyword = ({ setInputValue }: IProps) => {
+  const [keywordSelector, setKeywordSelector] = useState(0)
+
   const dispatch = useAppDispatch()
   const keyword = useSelector((state: RootState) => state.search.keyword)
   const { data } = useKeywordQuery(keyword)
@@ -21,6 +25,24 @@ const RecommendKeyword = ({ setInputValue }: IProps) => {
     setInputValue(e.currentTarget.value)
     dispatch(setKeyword(e.currentTarget.value))
   }
+
+  const keyEventHandler = useCallback(
+    (e: KeyboardEvent) => {
+      const max = data ? data.length : 0
+      if (e.key === 'ArrowUp') {
+        setKeywordSelector((prev) => (prev > 0 ? prev - 1 : prev))
+      } else if (e.key === 'ArrowDown') {
+        setKeywordSelector((prev) => (prev < max - 1 ? prev + 1 : prev))
+      }
+    },
+    [data]
+  )
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyEventHandler)
+
+    return () => window.removeEventListener('keydown', keyEventHandler)
+  }, [keyEventHandler])
 
   if (!keyword || !data) {
     return <div>검색어를 입력중 입니다.</div>
@@ -32,14 +54,24 @@ const RecommendKeyword = ({ setInputValue }: IProps) => {
 
   return (
     <ul>
-      {data.map((item) => (
-        <li key={item.sickCd}>
-          <SearchIcon />
-          <button type='button' value={item.sickNm} onClick={clickHandler}>
-            {item.sickNm}
-          </button>
-        </li>
-      ))}
+      {data.map((item, i) => {
+        const newItemNm = item.sickNm.replaceAll(keyword, `<mark>${keyword}<mark>`).split('<mark>')
+
+        return (
+          <li className={cx({ [styles.selector]: i === keywordSelector })} key={item.sickCd}>
+            <SearchIcon />
+            <button type='button' value={item.sickNm} onClick={clickHandler}>
+              {newItemNm.map((el) => {
+                if (el === keyword) {
+                  return <mark>{el}</mark>
+                }
+
+                return el
+              })}
+            </button>
+          </li>
+        )
+      })}
     </ul>
   )
 }
