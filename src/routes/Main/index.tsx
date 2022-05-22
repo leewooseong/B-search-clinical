ea5ad88c56b3018e9b49af690efebecac00307e2
leewooseong <sitpo96@naver.com>
2022-05-22 10:02:36 +0900

    refactor Main 및 RecommendKeyword css 수정, crlf lf관련 린트설정 추가

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,6 +1,7 @@
 import styles from './Main.module.scss'
 import { ChangeEvent, FormEvent, Suspense, useMemo, useState } from 'react'
 import { debounce } from 'lodash'
+import cx from 'classnames'
 
 import { useAppDispatch } from 'hooks/useAppDispatch'
 import { setKeyword } from 'states/search'
@@ -31,23 +32,22 @@ const Main = () => {
           <br /> 온라인으로 참여하기
         </h1>
         <form className={styles.formBox} onSubmit={onSubmitHandler}>
-          <div className={styles.searchBox}>
-            <SearchIcon className={styles.pc} />
-            <input type='text' value={inputValue} onChange={onChangeHandler} placeholder='질환명을 입력해주세요.' />
-          </div>
-          <button type='submit'>
-            <span className={styles.pc}>검색</span>
-            <SearchIcon className={styles.mobile} />
+          <SearchIcon className={cx(styles.forPcScreen, styles.searchIcon)} />
+          <input type='text' value={inputValue} onChange={onChangeHandler} placeholder='질환명을 입력해주세요.' />
+
+          <button type='submit' className={styles.searchButton}>
+            <span className={styles.forPcScreen}>검색</span>
+            <SearchIcon className={styles.forMobileScreen} />
           </button>
+          {inputValue && (
+            <div className={styles.listContainer}>
+              <p className={styles.listHeader}>추천 검색어</p>
+              <Suspense fallback={<p>로딩중</p>}>
+                <RecommendKeyword setInputValue={setInputValue} />
+              </Suspense>
+            </div>
+          )}
         </form>
-        {inputValue && (
-          <div className={styles.listContainer}>
-            <h2>추천 검색어</h2>
-            <Suspense fallback={<p>로딩중</p>}>
-              <RecommendKeyword setInputValue={setInputValue} />
-            </Suspense>
-          </div>
-        )}
       </main>
       <SectionSubscribe />
     </>
